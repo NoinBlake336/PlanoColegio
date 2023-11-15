49a60c4c66ae0b087f088b3b68263bc9bbe92ca9
NoinBlake336 <josechocobar@ipetym69.edu.ar>
2023-11-15 20:09:09 -0300

    adding modal

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const presentacion = document.getElementById('presentation')
 const planoColegio = document.getElementById('planoColegio')
 const plano = document.getElementById('plano')
 const main = document.getElementById('main');
+const modal = document.querySelector(".modal-container")
+const modalButton = document.querySelector("#close-button")
 botonVer.addEventListener("click",(e)=>{
     presentacion.classList.add('hidden')
     if(planoColegio.classList.contains('hidden')){
@@ -13,7 +15,9 @@ botonVer.addEventListener("click",(e)=>{
 
 
 
-
+modalButton.addEventListener("click", () =>{
+  modal.classList.add("hidden");
+})
 const posicionesEnBlanco = [1,5,6,9,10,13,14,17,18,21,22,25,26];
 const posicionAzul = [0,1,2,3,4,7,8,12,15,16,20,23,24,27,28,29,30,31,32,33,34,35,36]
 const indexes = [30,29,28,33,34]
@@ -42,12 +46,15 @@ const aulasAcceso = document.querySelectorAll(".acceso");
 aulasAcceso.forEach( aula => {
   aula.addEventListener("click", (e)=>{
     e.stopPropagation();
-    if(!e.target.id){
+    if(!e.target.id && e.target.parentElement.classList.contains("acceso")){
       console.log(e.target.parentElement.id);
+
+      modal.classList.remove("hidden")
 
     }
     else{
       console.log(e.target.id);
+      modal.classList.remove("hidden")
     }
 
   })
